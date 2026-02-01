174f75dd3c20552dfd600ea5733ff9c4ef376934
priyansh902 <pk953666@gmail.com>
2026-02-01 21:08:07 +0530

    User methods

---
+++ src/main/java/com/example/trunk/Model/User.java
@@ -0,0 +1,34 @@
+package com.example.trunk.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+}
---
+++ src/main/java/com/example/trunk/Repositiory/userRepositiory.java
@@ -0,0 +1,13 @@
+package com.example.trunk.Repositiory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.trunk.Model.User;
+
+
+@Repository
+public interface userRepositiory extends JpaRepository<User, Long> {
+
+    
+}
