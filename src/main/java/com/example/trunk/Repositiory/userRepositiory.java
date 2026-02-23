b84ed2b9cac738fbb8df29086fabc4f8ad7d40ae
Priyanshu Kumar <pk953666@gmail.com>
2026-02-23 08:23:10 +0530

    r

--- src/main/java/com/example/trunk/Repositiory/userRepositiory.java
+++ src/main/java/com/example/trunk/Repositiory/userRepositiory.java
@@ -1,5 +1,7 @@
 package com.example.trunk.Repositiory;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,6 @@ import com.example.trunk.Model.User;
 public interface userRepositiory extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    
     
 }
